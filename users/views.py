6ee5d701bb141b8a43bda49ad23a42ccedea6fb7
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-10 15:30:22 +0500

    Add perform_create + perform_update for UserViewSet

--- users/views.py
+++ users/views.py
@@ -10,3 +10,19 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        """
+        Хэширует создаваемый при регистрации пароль.
+        """
+        instance = serializer.save(is_active=True)
+        instance.set_password(instance.password)
+        instance.save()
+
+    def perform_update(self, serializer):
+        """
+        Хэширует редактируемый пароль.
+        """
+        instance = serializer.save()
+        instance.set_password(instance.password)
+        instance.save()
