b5e29d95472ad59155c4e8f9f9c22966eeb98b54
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-10 15:19:26 +0500

    Add routers for TokenObtainPairView + TokenRefreshView

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
+from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserViewSet
@@ -9,5 +11,6 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
