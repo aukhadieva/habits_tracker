c4a397e5c85808fd4feb1d5977933c9c11dd722a
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-10 14:06:11 +0500

    Add User model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='эл. почта')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
