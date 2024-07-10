0338cdf07ce4c4706c3b32f707a77c978d9b68d1
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-10 15:27:59 +0500

    Add permission_classes for TokenObtainPairView + TokenRefreshView

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework import routers
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
@@ -11,6 +12,6 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(permission_classes=[AllowAny]), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(permission_classes=[AllowAny]), name='token_refresh'),
 ] + router.urls
