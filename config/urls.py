686ef8b79f01e9b50fa9df7a9ff01764dce0559a
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-11 11:45:07 +0500

    Include habits.urls

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ schema_view = views.get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls', namespace='users')),
+    path('habits/', include('habits.urls', namespace='habits')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
