6256c8c2c5ed4014933c6a9dcbd1c94be34ea8ee
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-11 12:46:26 +0500

    Add router for HabitPublicListAPIView

--- habits/urls.py
+++ habits/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 
 from habits.apps import HabitsConfig
-from habits.views import (HabitCreateAPIView, HabitUpdateAPIView, HabitListAPIView, HabitRetrieveAPIView,
-                          HabitDestroyAPIView)
+from habits.views import (HabitCreateAPIView, HabitUpdateAPIView, HabitPrivetListAPIView, HabitRetrieveAPIView,
+                          HabitDestroyAPIView, HabitPublicListAPIView)
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
     path('create_habit/', HabitCreateAPIView.as_view(), name='create_habit'),
     path('edit_habit/<int:pk>/', HabitUpdateAPIView.as_view(), name='edit_habit'),
-    path('', HabitListAPIView.as_view(), name='habits'),
+    path('', HabitPrivetListAPIView.as_view(), name='habits'),
+    path('public_habits/', HabitPublicListAPIView.as_view(), name='habits'),
     path('habit/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit'),
     path('delete_habit/<int:pk>/', HabitDestroyAPIView.as_view(), name='delete_habit')
 ]
