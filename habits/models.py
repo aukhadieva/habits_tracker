30a3d84986829c85ca157f5fe66003dffdf4f8d0
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-11 11:42:04 +0500

    Add Habit model

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+NULLABLE = {'blank': True, 'null': True}
+HABIT_FREQUENCY = [
+    ('daily', 'ежедневно'),
+    ('once_a_week', 'раз в неделю'),
+]
+
+
+class Habit(models.Model):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Пользователь', **NULLABLE)
+    behavior = models.CharField(max_length=200, verbose_name='Действие',
+                                db_comment='действие, которое представляет собой привычка')
+    time = models.TimeField(verbose_name='Время', db_comment='время, когда необходимо выполнять привычку')
+    location = models.CharField(max_length=200, verbose_name='Место')
+
+    related_habit = models.ForeignKey(to='self', on_delete=models.SET_NULL,
+                                      verbose_name='Связанная привычка', **NULLABLE)
+    frequency = models.CharField(choices=HABIT_FREQUENCY, default=HABIT_FREQUENCY[0][0], verbose_name='Периодичность',
+                                 db_comment='периодичность выполнения привычки для напоминания в днях', **NULLABLE)
+    award = models.CharField(max_length=200, verbose_name='Вознаграждение', **NULLABLE)
+    duration = models.DateTimeField(verbose_name='Время на выполнение',
+                                    db_comment='время, которое предположительно потратит пользователь '
+                                               'на выполнение привычки', **NULLABLE)
+
+    is_pleasant_habit = models.BooleanField(default=False, verbose_name='Признак приятной привычки')
+    is_public = models.BooleanField(verbose_name='Признак публичности', **NULLABLE)
+
+    def __str__(self):
+        return (f'Привычка {self.behavior} пользователя {self.owner} '
+                f'(признак приятной привычки: {self.is_pleasant_habit})')
+
+    class Meta:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
+        ordering = ('is_pleasant_habit', 'is_public',)
