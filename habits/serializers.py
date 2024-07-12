f8e792ddd05c48c300f7a514c014c35dcda4e208
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-12 09:48:34 +0500

    Update HabitSerializers (add validators)

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import DurationValidator, RelatedHabitValidator, AwardValidator, FrequencyValidator
 
 
 class HabitSerializers(serializers.ModelSerializer):
@@ -8,3 +9,5 @@ class HabitSerializers(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+        validators = [DurationValidator(field='duration'), RelatedHabitValidator(field='related_habit'),
+                      AwardValidator(field='award'), FrequencyValidator(field='frequency')]
