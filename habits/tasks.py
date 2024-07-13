a999e1dbc7b17acd54e3e370ac67b3b35ad0753d
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-13 16:19:08 +0500

    Add send_reminder shared_task

---
+++ habits/tasks.py
@@ -0,0 +1,28 @@
+from celery import shared_task
+from django.utils import timezone
+
+from habits.models import Habit
+from habits.services import send_message
+
+
+@shared_task
+def send_reminder():
+    """
+    Отложенная задача для напоминания о том, в какое время
+    и какие привычки необходимо выполнять.
+    Также удаляет привычку, если прошло 7 напоминаний.
+    """
+    current_time = timezone.now().strftime('%X')
+    habits = Habit.objects.all()
+
+    for habit in habits:
+        if habit.time == current_time and habit.frequency > 0:
+            text = (
+                f'Итс тайм! Выполни "{habit.behavior}" в локации {habit.location} '
+                f'и получи {habit.award if habit.award else (habit.related_habit if habit.related_habit
+                                                             else "хорошее настроение")}!')
+
+            chat_id = habit.owner.tg_chat_id
+            send_message(text, chat_id)
+            habit.frequency -= 1
+            habit.save()
