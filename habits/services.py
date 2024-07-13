2d496f3af0ad9a9f42a44626c2bda9d7663ca98a
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-13 11:44:37 +0500

    Add send_message service func (tg bot)

---
+++ habits/services.py
@@ -0,0 +1,14 @@
+import requests
+
+from config import settings
+
+
+def send_message(text, chat_id):
+    """
+    Сервисная функция по отправке сообщений телеграм-ботом.
+    """
+    params = {
+        'text': text,
+        'chat_id': chat_id,
+    }
+    requests.get(f'{settings.TG_URL}{settings.TGBOT_TOKEN}/sendMessage', params=params)
