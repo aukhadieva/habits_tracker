a383b455450f4b17c24da6cf69bed0cf9261fcd4
Olga Aukhadieva <sarole4ka@gmail.com>
2024-07-13 11:42:43 +0500

    Add tg_chat_id field to User model

--- users/models.py
+++ users/models.py
@@ -6,5 +6,7 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='эл. почта')
 
+    tg_chat_id = models.CharField(max_length=50, verbose_name='телеграм chat-id')
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
